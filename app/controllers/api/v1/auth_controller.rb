93b81668a28e70b44946e02fb345941b7e9ae0b5
Marc De Biolley <marcdebiolley@github.com>
2025-11-06 09:31:16 +0100

    Auth: fix callbacks; prod MySQL

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::AuthController < ApplicationController
+  skip_before_action :require_user!, only: [ :signup, :login ], raise: false
   def signup
     email = params[:email].to_s.downcase
     password = params[:password].to_s
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::API
   include Authentication
   before_action :require_user!
-
-  private
-  def require_user!
-    if respond_to?(:current_user, true) && current_user
-      return
-    end
-
-    render json: { error: 'unauthorized' }, status: :unauthorized
-  end
 end
